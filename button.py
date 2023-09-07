990607f5da3853369b0613a21d2b7e4122443957
Christian DerManuelian <129346834+chris-derman03@users.noreply.github.com>
2023-09-07 12:51:10 -0700

    Update button.py
    Added comments to button.py

--- button.py
+++ button.py
@@ -1,4 +1,5 @@
-# Class for buttons
+# Class for a button
+# Has a method the show the button screen and to determine if it collided with some given cursor position.
 class Button():
 
     # Set the position and image for a button
@@ -19,7 +20,11 @@ class Button():
     def is_clicked(self, cursor_position):
 
         return self.rect.collidepoint(cursor_position)
-    
+
+
+
+# Type of button that inherets from our normal button
+# This type of button will only be able to clicked once after it is drawn to the screen
 class ManagedButton(Button):
 
     did_draw = False
@@ -28,21 +33,30 @@ class ManagedButton(Button):
     def __init__(self, x, y, image):
         super().__init__(x, y, image)
 
+    # Only when we draw the button to the screen should it be allowed to be clicked
     def draw(self, screen):
         super().draw(screen)
         self.did_draw = True
 
-    # If it drew the frame and is clicked
+    # Same as the detecting a click on the regular button
+    # but this time only detect a click if the button was drawn to the frame
     def is_clicked(self, cursor_position):
         return self.did_draw and super().is_clicked(cursor_position)
+
+
+
         
-        
+
+# Framework to keep track of all of our buttons
+# As of now, only ManagedButton instances will go into this
 class ButtonManager():
 
-    # List of buttons
+    # List of buttons on constructor
     def __init__(self):
         self.buttons = []
 
+    # Prevent all buttons from being able to be clicked
+    # This should be called at the beginning of every frame/gameloop tick
     def reset(self):
         for button in self.buttons:
             button.did_draw = False
